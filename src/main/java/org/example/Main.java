efbcaac2224b69d4dfd214939cd247847bde6fbd
Gulliver Cristobal <cristobal.gulliver13@gmail.com>
2024-05-30 16:43:29 +0800

    ArrayList

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,27 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+        /**
+     * PSVM
+     */
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!\n");
+        ArrayList<String> people = new ArrayList<>();
+        people.add("Melares");
+        people.add("Srolef");
+        people.add("Douppy");
+        people.remove("Douppy");
+        System.out.println("people = " + people);
+        // output: people = [Melares, Srolef]
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        ArrayList<String> human = new ArrayList<>();
+        // addAll Adds the contents of people to human
+        human.addAll(people);
+        System.out.println("human = " + human);
+        // output: human = [Melares, Srolef]
     }
 }
