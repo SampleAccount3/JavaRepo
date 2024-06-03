a1d424fe5933a05ed5deb07fd421424f4cb9579c
Gulliver Cristobal <cristobal.gulliver13@gmail.com>
2024-06-03 17:50:27 +0800

    Done with the Flowable

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,77 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import io.reactivex.rxjava3.core.BackpressureStrategy;
+import io.reactivex.rxjava3.core.Flowable;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.Scheduler;
+import io.reactivex.rxjava3.functions.Function;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
+import static java.lang.Thread.sleep;
+
+/**
+ * BackPressure Strategy:
+ * Missing - no BackPressure Strategy
+ * Error - downStream throws an exception right away when it cannot keep up with the source
+ * Buffer - items are buffered until downstream is able to handle it
+ * Latest - last emission is kept until downstream is able to handle it
+ * Drop - when downstream cannot handle new emissions, all of these emissions are discarded
+ *
+ * can be applied using Flowable.create(source, strategy)
+ * can be applied using onBackpressureX (X - Missing, Drop etc.)
+ */
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+//        synchronousObservableExample();
+//        asynchronousObservableExample();
+        asyncFlowableExample();
+    }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    private static void asyncFlowableExample() {
+        Flowable.range(1,2048)
+                .map(Item::new)
+                .observeOn(Schedulers.io())
+                .subscribe( item -> {
+                    sleep(100);
+                    System.out.println("Received my Item: " + item.id + "\n");
+                        });
+        try {
+            sleep(Long.MAX_VALUE);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
+    }
+
+    public static void synchronousObservableExample(){
+         Observable.range(1,10000)
+                 .map(Item::new)
+                 .subscribe(item -> {
+                     sleep(1000);
+                     System.out.println("Received my Item" + item.id + "\n");
+                 });
+
+     }
+
+     public static void asynchronousObservableExample(){
+        Observable.range(1,10)
+                .map(Item::new)
+                .observeOn(Schedulers.io())
+                .subscribe(item ->{
+                    sleep(1000);
+                    System.out.println("Received MyItem: " + item.id + "\n");
+                });
+         try {
+             sleep(Long.MAX_VALUE);
+         } catch (InterruptedException e) {
+             throw new RuntimeException(e);
+         }
+     }
+}
+
+class Item{
+    int id;
+    public Item(int id){
+        this.id = id;
+        System.out.println("Item is Created: " + id);
     }
 }
