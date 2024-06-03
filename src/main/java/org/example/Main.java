bd79d678efcb1c36d0f511455a0da945519ea8ab
Gulliver Cristobal <cristobal.gulliver13@gmail.com>
2024-06-03 15:51:02 +0800

    init Commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,40 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import io.reactivex.rxjava3.annotations.NonNull;
+import io.reactivex.rxjava3.core.Completable;
+import io.reactivex.rxjava3.core.CompletableObserver;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.functions.Action;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Completable completable = CreateCompletable();
+        completable.subscribe(new CompletableObserver() {
+            @Override
+            public void onSubscribe(@NonNull Disposable d) {
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+            }
+            @Override
+            public void onComplete() {
+                System.out.println("Operation is complete");
+            }
+            @Override
+            public void onError(@NonNull Throwable e) {
+
+            }
+        });
+    }
+
+    private static Completable CreateCompletable() {
+        return Completable.fromAction(DeleteDatabaseFromDBAction());
+    }
+
+    private static Action DeleteDatabaseFromDBAction() {
+        return new Action() {
+            @Override
+            public void run() throws Throwable {
+                System.out.println("Deleting item from database");
+            }
+        };
     }
 }
