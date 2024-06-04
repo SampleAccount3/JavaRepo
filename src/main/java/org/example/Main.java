05979359c00a285fb044ce17b145a407052b19b5
Gulliver Cristobal <cristobal.gulliver13@gmail.com>
2024-06-04 09:18:48 +0800

    Done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,46 @@
 package org.example;
 
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.observables.ConnectableObservable;
+
+import java.util.concurrent.TimeUnit;
+
 public class Main {
 
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws InterruptedException {
+//        coldObserverSample();
+        hotObserverSample();
+    }
+
+    /** for each observable continue receiving the items
+     *
+     * @throws InterruptedException
+     */
+    private static void hotObserverSample() throws InterruptedException {
+        ConnectableObservable observable = Observable.interval(1, TimeUnit.SECONDS)
+                .publish();
+
+        observable.connect();
+
+        observable.subscribe((item) -> {
+            System.out.println("Observer 1, sec: " + item);
+        });
+        Thread.sleep(5000);
+        observable.subscribe((item) -> {
+            System.out.println("Observer 2, sec: " + item);
+        });
+        Thread.sleep(5000);
+    }
+
+    /** for each subscriber it emits the same items
+     *
+     */
+    private static void coldObserverSample() {
+        Observable<String> observable = Observable.just("a","b","c");
+
+        observable.subscribe(item-> System.out.println("Observer 1 " + item));
+        observable.subscribe(item-> System.out.println("Observer 2 " + item));
+        observable.subscribe(item-> System.out.println("Observer 3 " + item));
     }
 
 }
