097e3d0fb39c59bb3e1aff6b9a0a7312844d2d2b
Gulliver Cristobal <cristobal.gulliver13@gmail.com>
2024-06-05 10:49:41 +0800

    init commit

---
+++ src/main/java/org/example/FuncInterface.java
@@ -0,0 +1,11 @@
+package org.example;
+
+interface FuncInterface {
+    void abstractFun(int x);
+}
+interface SampleInterface{
+    int sum(int x, int y);
+}
+interface NoArgInterface {
+    String Hello();
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,18 @@
 package org.example;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class Main {
 
     public static void main(String[] args) {
-        
+        FuncInterface fObj = (int x) -> System.out.println(x+2);
+        SampleInterface sampleInterface = Integer::sum;
+        System.out.println(sampleInterface.sum(20,10));
+        NoArgInterface noArgInterface = ()->{
+            return "Hello";
+        };
+        System.out.println(noArgInterface.Hello());
+
     }
 
 }
