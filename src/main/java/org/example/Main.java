77181926794a104b5e6d377ad491d499284be1d5
Gulliver Cristobal <cristobal.gulliver13@gmail.com>
2024-06-10 15:33:24 +0800

    Init Commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,23 @@ package org.example;
 public class Main {
 
     public static void main(String[] args) {
-        
+        SampleThreads sampleThreads = new SampleThreads();
+        ThreadGroup tg = new ThreadGroup("Group A");
+        Thread t1 = new Thread(tg, sampleThreads);
+        Thread t2 = new Thread(tg, sampleThreads);
+        Thread t3 = new Thread(tg, sampleThreads);
+        Thread t4 = new Thread(tg, sampleThreads);
+        Thread t5 = new Thread(tg, sampleThreads);
+
+       tg.list();
     }
 
+}
+
+class SampleThreads implements Runnable{
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName());
+    }
 }
