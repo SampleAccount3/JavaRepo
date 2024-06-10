ecfce1e1e3d4540032d8fe26432d337433a0f5bf
Gulliver Cristobal <cristobal.gulliver13@gmail.com>
2024-06-10 14:45:45 +0800

    Init Commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,43 +1,58 @@
 package org.example;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Main {
+    static final int MAX_THREAD = 3;
 
     public static void main(String[] args) {
-//        SampleThreadsFunc();
-//       sampleRunnableFunc();
-
+//        executorService1();
+//        sampleThreadsMethod();
+//        sampleRunnableMethod();
+        sampleThreadPool();
     }
 
-    private static void sampleRunnableFunc() {
+    private static void sampleThreadPool() {
+        Runnable rb1 = new Task("Task 1");
+        Runnable rb2 = new Task("Task 2");
+        Runnable rb3 = new Task("Task 3");
+        Runnable rb4 = new Task("Task 4");
+        Runnable rb5 = new Task("Task 5");
+
+        ExecutorService executorService = Executors.newFixedThreadPool(MAX_THREAD);
+
+        executorService.execute(rb1);
+        executorService.execute(rb2);
+        executorService.execute(rb3);
+        executorService.execute(rb4);
+        executorService.execute(rb5);
+
+        executorService.shutdown();
+    }
+
+    public static void sampleRunnableMethod(){
         SampleRunnable sampleRunnable = new SampleRunnable();
-        Thread threadObj = new Thread(sampleRunnable);
-
-        System.out.println(threadObj.getState());
-        threadObj.start();
-        System.out.println(threadObj.getState());
-
-        try {
-            threadObj.join();
-            System.out.println(threadObj.getState());
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+        Thread t2 = new Thread(sampleRunnable);
+        t2.start();
     }
-
-    private static void SampleThreadsFunc() {
+    public static void sampleThreadsMethod(){
         SampleThreads sampleThreads = new SampleThreads();
-        System.out.println("Sample1");
-        System.out.println(sampleThreads.getState());
         sampleThreads.start();
-        System.out.println("Starting");
-
-        try {
-            sampleThreads.join();
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        System.out.println(sampleThreads.getState());
     }
 
+    public static void executorService1(){
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+
+        for (int i = 0; i < 10 ; i++) {
+            Runnable worker = new WorkerThread(" " + i);
+            executorService.execute(worker);
+        }
+        executorService.shutdown();
+        while (!executorService.isTerminated()){
+
+        }
+        System.out.println("Finished All Threads");
+    }
 
 }
--- src/main/java/org/example/SampleThreads.java
+++ src/main/java/org/example/SampleThreads.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 class SampleThreads extends Thread{
     @Override
     public void run() {
@@ -17,6 +20,60 @@ class SampleRunnable implements Runnable{
     public void run() {
         System.out.println("The thread is Running");
     }
+}
 
+class WorkerThread implements Runnable{
+    private String message;
+    public WorkerThread(String message) {
+        this.message = message;
+    }
+    public String getMessage() {
+        return message;
+    }
+    public void setMessage(String message){
+        this.message = message;
+    }
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName() + " Thread = " + message);
+        processMessage();
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+        System.out.println(Thread.currentThread().getName() + "End");
+    }
+    private void processMessage() {
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
 
+class Task implements Runnable{
+    private String taskName;
+    public Task(String taskName) {
+        this.taskName = taskName;
+    }
+    @Override
+    public void run() {
+        try{
+            for (int i = 0; i <5 ; i++) {
+                if (i == 0){
+                    Date dt = new Date();
+                    SimpleDateFormat sdf = new SimpleDateFormat("hh : mm : ss");
+                    System.out.println("Initialization time for the task name" + taskName + " = " + sdf.format(dt));
+
+                }else{
+                    Date dt = new Date();
+                    SimpleDateFormat sdf = new SimpleDateFormat("hh : mm : ss");
+                    System.out.println("Time of execution for the task name: " + taskName + " = " +sdf.format(dt));
+                }
+                Thread.sleep(2000);
+            }
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
 }
