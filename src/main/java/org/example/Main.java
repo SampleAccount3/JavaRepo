7c32e8df6760288be6de4cefb19fae2e57419edf
Gulliver Cristobal <cristobal.gulliver13@gmail.com>
2024-06-19 09:01:23 +0800

    Test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,102 +1,140 @@
 package org.example;
 
-// Java program that prints the odd and even numbers using two threads.
-// the time complexity of the program is O(N), where N is the number up to which we   
-// are displaying the numbers  
-public class Main
-{
-    // Starting the counter
-    int contr = 1;
-    static int NUM;
-    // Method for printing the odd numbers
-    public void displayOddNumber()
-    {
-// note that synchronized blocks are necessary for the code for getting the desired   
-// output. If we remove the synchronized blocks, we will get an exception.  
-        synchronized (this)
-        {
-// Printing the numbers till NUM  
-            while (contr < NUM)
-            {
-// If the contr is even then display  
-                while (contr % 2 == 0)
-                {
-// handling the exception handle  
-                    try
-                    {
-                        wait();
-                    }
-                    catch (InterruptedException ex)
-                    {
-                        ex.printStackTrace();
-                    }
-                }
-// Printing the number  
-                System.out.print(contr + " ");
-// Incrementing the contr   
-                contr = contr + 1;
-// notifying the thread which is waiting for this lock   
-                notify();
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.lang.reflect.Type;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class Main{
+    public static void main(String[] args) {
+//        JsonObjects();
+        GsonObjects();
+
+//        System.out.println(map.get("details"));
+    }
+
+    private static void GsonObjects() {
+        String jsonString = "{\n" +
+                "    \"header\": [\n" +
+                "        {\n" +
+                "            \"id\": 1,\n" +
+                "            \"order_id\": \"D03 TB MASTERS BERNA BEACH\",\n" +
+                "            \"branch\": \"WHO\",\n" +
+                "            \"submitted_by\": \"JOBERTZON STA. CATALINA\",\n" +
+                "            \"salesman\": null,\n" +
+                "            \"status\": \"pending\",\n" +
+                "            \"date_submitted\": \"2024-06-10 15:28:57\",\n" +
+                "            \"created_at\": \"2024-06-10T07:28:57.000000Z\",\n" +
+                "            \"updated_at\": \"2024-06-10T07:28:57.000000Z\",\n" +
+                "            \"details\": [\n" +
+                "                {\n" +
+                "                    \"id\": 1,\n" +
+                "                    \"order_id\": 1,\n" +
+                "                    \"line_number\": \"1\",\n" +
+                "                    \"store_loc\": \"D03\",\n" +
+                "                    \"qty\": \"1\",\n" +
+                "                    \"uom\": \"SET\",\n" +
+                "                    \"barcode\": \"147200005134\",\n" +
+                "                    \"desc\": \"POZZI/BTUB PALM SPRING (BN-406\",\n" +
+                "                    \"created_at\": \"2024-06-10T07:28:57.000000Z\",\n" +
+                "                    \"updated_at\": \"2024-06-10T07:28:57.000000Z\"\n" +
+                "                }\n" +
+                "            ]\n" +
+                "        }\n" +
+                "    ]\n" +
+                "}";
+
+        // Parse JSON string to Map<String, Object>
+        Gson gson = new Gson();
+        Type type = new TypeToken<Map<String, Object>>(){}.getType();
+        Map<String, Object> data = gson.fromJson(jsonString, type);
+
+        // Access header array
+        List<Map<String, Object>> headerList = (List<Map<String, Object>>) data.get("header");
+        if (headerList != null && !headerList.isEmpty()) {
+            // Get the first header item
+            Map<String, Object> firstHeaderItem = headerList.get(0);
+
+            // Access details array within the first header item
+            List<Map<String, Object>> detailsList = (List<Map<String, Object>>) firstHeaderItem.get("details");
+            if (detailsList != null && !detailsList.isEmpty()) {
+                // Get the first details item
+                Map<String, Object> detailsItem = detailsList.get(0);
+                System.out.println("First details item:");
+                System.out.println("line_number: " + detailsItem.get("line_number"));
+                System.out.println("store_loc: " + detailsItem.get("store_loc"));
+                // Access other fields as needed
             }
         }
     }
-    // Method for printing the even numbers
-    public void displayEvenNumber()
-    {
-        synchronized (this)
-        {
-// Printing the number till NUM  
-            while (contr < NUM)
-            {
-// If the count is odd then display  
-                while (contr % 2 == 1)
-                {
-// handling the exception  
-                    try
-                    {
-                        wait();
-                    }
-                    catch (InterruptedException ex)
-                    {
-                        ex.printStackTrace();
-                    }
-                }
-// Printing the number  
-                System.out.print(contr + " ");
-// Incrementing the contr  
-                contr = contr +1;
-// Notifying to the 2nd thread  
-                notify();
-            }
+
+    public static void JsonJackson(){
+        String jsonString = "{ \"header\": [ { \"id\": 1, \"order_id\": \"D03 TB MASTERS BERNA BEACH\", \"branch\": \"WHO\", \"submitted_by\": \"JOBERTZON STA. CATALINA\", \"salesman\": null, \"status\": \"pending\", \"date_submitted\": \"2024-06-10 15:28:57\", \"created_at\": \"2024-06-10T07:28:57.000000Z\", \"updated_at\": \"2024-06-10T07:28:57.000000Z\", \"details\": [ { \"id\": 1, \"order_id\": 1, \"line_number\": \"1\", \"store_loc\": \"D03\", \"qty\": \"1\", \"uom\": \"SET\", \"barcode\": \"147200005134\", \"desc\": \"POZZI/BTUB PALM SPRING (BN-406\", \"created_at\": \"2024-06-10T07:28:57.000000Z\", \"updated_at\": \"2024-06-10T07:28:57.000000Z\" } ] } ] }";
+
+        ObjectMapper mapper = new ObjectMapper();
+        HashMap<String, Object> map = null;
+        try {
+            map = mapper.readValue(jsonString, HashMap.class);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+        // Get the "header" list from the map
+        List<Map<String, Object>> headerList = (List<Map<String, Object>>) map.get("header");
+
+        // Get the first header item (since it's an array)
+        if (headerList != null && !headerList.isEmpty()) {
+            Map<String, Object> firstHeaderItem = headerList.get(0);
+            List<Map<String, Object>> detailsList = (List<Map<String, Object>>) firstHeaderItem.get("details");
+            Map<String, Object> detailsItem = detailsList.get(0);
+
+            // Get the "details" list from the first header item
+
+            System.out.println("----------------header----------------");
+            System.out.println(firstHeaderItem.get("date_submitted"));
+            System.out.println("----------------details----------------");
+            System.out.println(detailsItem.get("store_loc"));
+        } else {
+            System.out.println("Header list is empty or null.");
         }
     }
-    // main method
-    public static void main(String[] argvs)
-    {
-// The NUM is given  
-        NUM = 20;
-// creating an object of the class OddEvenExample  
-        Main oe = new Main();
-// creating a thread th1  
-        Thread th1 = new Thread(new Runnable()
-        {
-            public void run()
-            {
-// invoking the method displayEvenNumber() using the thread th1  
-                oe.displayEvenNumber();
+
+    public static void JsonObjects(){
+        String jsonString = "{ \"header\": [ { \"id\": 1, \"order_id\": \"D03 TB MASTERS BERNA BEACH\", \"branch\": \"WHO\", \"submitted_by\": \"JOBERTZON STA. CATALINA\", \"salesman\": null, \"status\": \"pending\", \"date_submitted\": \"2024-06-10 15:28:57\", \"created_at\": \"2024-06-10T07:28:57.000000Z\", \"updated_at\": \"2024-06-10T07:28:57.000000Z\", \"details\": [ { \"id\": 1, \"order_id\": 1, \"line_number\": \"1\", \"store_loc\": \"D03\", \"qty\": \"1\", \"uom\": \"SET\", \"barcode\": \"147200005134\", \"desc\": \"POZZI/BTUB PALM SPRING (BN-406\", \"created_at\": \"2024-06-10T07:28:57.000000Z\", \"updated_at\": \"2024-06-10T07:28:57.000000Z\" } ] } ] }";
+
+        JSONObject jsonObject = new JSONObject(jsonString);
+
+        // Get the "header" array from the JSON object
+        JSONArray headerArray = jsonObject.getJSONArray("header");
+
+        // Get the first header item (since it's an array)
+        if (headerArray.length() > 0) {
+            JSONObject firstHeaderItem = headerArray.getJSONObject(0);
+
+            // Get the "details" array from the first header item
+            JSONArray detailsArray = firstHeaderItem.getJSONArray("details");
+
+            // Get the first details item (since it's an array)
+            if (detailsArray.length() > 0) {
+                JSONObject detailsItem = detailsArray.getJSONObject(0);
+
+                // Print values
+                System.out.println("----------------header----------------");
+                System.out.println(firstHeaderItem.getString("date_submitted"));
+                System.out.println("----------------details----------------");
+                System.out.println(detailsItem.getString("store_loc"));
+            } else {
+                System.out.println("Details array is empty.");
             }
-        });
-// creating a thread th2  
-        Thread th2 = new Thread(new Runnable()
-        {
-            public void run()
-            {
-// invoking the method displayOddNumber() using the thread th2  
-                oe.displayOddNumber();
-            }
-        });
-// starting both of the threads  
-        th1.start();
-        th2.start();
+        } else {
+            System.out.println("Header array is empty.");
+        }
     }
-}  
+
+}
