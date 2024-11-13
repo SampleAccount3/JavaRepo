ecbd9cdd54dcae427a0a1901281642c8541dc744
Gulliver Cristobal <cristobal.gulliver13@gmail.com>
2024-11-13 10:23:01 +0800

    Init Commit

---
+++ src/main/java/org/example/ISampleAble.java
@@ -0,0 +1,5 @@
+package org.example;
+
+public interface ISampleAble <T> {
+    T Start();
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,43 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
 public class Main {
 
     public static void main(String[] args) {
-        
+        ISampleAble<String> IsampleAbleString;
+        List<String> names = new ArrayList<>();
+
+        names.add("Melares");
+        names.add("Melanio");
+        names.add("Kape");
+        names.add("Tablet");
+
+        Predicate<String> samplePredicate = s -> s.startsWith("M");
+        ISampleAble<String> IsampleAbleStringValue = new ISampleAble<String>() {
+            @Override
+            public String Start() {
+                return "Hello World";
+            }
+        } ;
+//        interfaceInAFunction(IsampleAbleStringValue);
+        predicateInAFunction(names);
+        System.out.println();
     }
+
+    private static List<String> predicateInAFunction(List<String> names) {
+        return names.stream()
+                .filter(x -> x.startsWith("K"))
+                .collect(Collectors.toList());
+    }
+
+    private static void interfaceInAFunction(ISampleAble iSampleAble) {
+        System.out.println(iSampleAble.Start());
+    }
+
 
 }
