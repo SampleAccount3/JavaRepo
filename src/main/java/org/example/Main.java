f9265df792e94a321631f80743a381fc586d5406
Gulliver Cristobal <cristobal.gulliver13@gmail.com>
2024-11-15 13:14:45 +0800

    init Commit With Updated Grade:

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,25 @@
 package org.example;
 
+import java.util.Optional;
+import java.util.OptionalInt;
+import java.util.stream.IntStream;
+
 public class Main {
 
     public static void main(String[] args) {
-        
-    }
+        Optional<String>  sampleObject = optionalSample("Melares");
+        System.out.println(sampleObject.isPresent());
+        OptionalInt reduced =
+                IntStream.range(1, 101).reduce((a, b) -> a + b);
 
+        System.out.println(reduced);
+//        System.out.println(sampleObject.get().getName());
+
+    }
+    private static Optional<String>  optionalSample(String name) {
+        SampleObject sample = new SampleObject();
+        sample.setName(null);
+//        return Optional.ofNullable(sample);
+        return Optional.ofNullable(sample.getName());
+    }
 }
---
+++ src/main/java/org/example/SampleObject.java
@@ -0,0 +1,24 @@
+package org.example;
+
+public class SampleObject {
+    String name;
+    String job;
+    public SampleObject() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+}
