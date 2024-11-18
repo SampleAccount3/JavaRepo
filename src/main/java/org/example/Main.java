f23e6f163a2cfefe94a9961f2bc4c5b4e5780ae7
Gulliver Cristobal <cristobal.gulliver13@gmail.com>
2024-11-18 16:16:03 +0800

    Init COmmit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,73 +6,52 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
+import java.util.function.Supplier;
 
 public class Main {
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
-
+    public static void main(String[] args){
         APIRequestService apiRequestService = new APIRequestService();
         RetrofitService retrofitService = new RetrofitService();
         CompletableFutureDriver completableFutureDriver = new CompletableFutureDriver();
 
-        APIComponent apiComponent = new APIComponent(apiRequestService,completableFutureDriver,retrofitService);
+        Supplier<String> sample = ()-> "This is a Sample";
 
-        ICallbackAble<String> sampleCallback = x -> x + "Sample";
-        CallbackDriver<String> sample = new CallbackDriver<>(sampleCallback, "This is a Sample");
-//        System.out.println(sample.executeCallback());
-
-        CompletableFuture<String> getUserName = completableFutureDriver.getUserName();
-        CompletableFuture<String> getAge = completableFutureDriver.getUserAge();
-        CompletableFuture<List<HashMap<String, Object>>> getAPISamples = completableFutureDriver.getAPISamples();
-
+        APIComponent apiComponent = new APIComponent(apiRequestService, completableFutureDriver, retrofitService);
         CompletableFuture<List<HashMap<String, Object>>> omnibusApiService = apiComponent.getOmnibusData();
 
-        omnibusApiService.thenApply(result ->{
-//            System.out.println("API Call result" + result);
-            return result;
-        }).exceptionally(ex->{
+        omnibusApiService.thenApply(result -> result).exceptionally(ex->{
             System.err.println("Error: " + ex.getMessage());
             return null;
         }).join();
         CompletableFuture<List<HashMap<String, Object>>> omnibusApiService2 = apiComponent.getOmnibusData();
-
-        omnibusApiService2.thenApply(result ->{
-//            System.out.println("API Call result" + result);
-            return result;
-        }).exceptionally(ex->{
+        omnibusApiService2.thenApply(result -> result).exceptionally(ex->{
             System.err.println("Error: " + ex.getMessage());
             return null;
         }).join();
 
         List<List<HashMap<String,Object>>> sampleJoin = List.of(omnibusApiService.join(),omnibusApiService2.join());
-//        System.out.println(sampleJoin);
 
+        sampleJoin.stream().forEach(System.out::println);
         Gson gson = new Gson();
         String json = gson.toJson(sampleJoin);
-        System.out.println(json);
-//        System.out.println(omnibusApiService.join()+": Sample");
-//        System.out.println(omnibusApiService2.join()+": Sample2");
-//        CompletableFuture<String> combinedFuture = getUserName
-//                .thenCombine(getAge, (userName, age) -> {
-//                    System.out.println("User UserName: " + userName);
-//                    System.out.println("User Age: " + age);
-//                    return "Combined Result: " + userName + "&" + age;
-//                })
-//                .thenCombineAsync(getAPISamples, (combinedResult, apiSamples) -> {
-//                    System.out.println("Combined Result: " + combinedResult);
-//                    System.out.println("API Samples:");
-//                    for (HashMap<String, Object> sampleHash : apiSamples) {
-//                        System.out.println(sampleHash);
-//                    }
-//                    return "Final Result: " + combinedResult + " with " + apiSamples.size() + " API samples.";
-//                })
-//                .thenCombineAsync(apiComponent.getOmnibusData(),(omnibusData,apiSamples) -> omnibusData)
-//                .exceptionally(error -> {
-//                    System.err.println("Error: " + error.getMessage());
-//                    return null;
-//                });
-//        System.out.println(combinedFuture.get() + "Done");
+//        System.out.println(json);
 
-        Thread.sleep(1000);
+        System.out.println("Finished");
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        CompletableFuture<String> sampleCompletable = CompletableFuture.supplyAsync(sample);
+
+        try {
+            System.out.println(sampleCompletable.get());
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
